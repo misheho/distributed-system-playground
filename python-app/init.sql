d84b2e112d6d9fdd441664dcf1c2c52460352e06
misheho <minitucna@centrum.cz>
2026-07-04 22:50:48 +0200

    initial db data updated

--- python-app/init.sql
+++ python-app/init.sql
@@ -6,11 +6,9 @@ CREATE TABLE IF NOT EXISTS quotes (
 );
 
 -- Insert sample data
-INSERT INTO quotes (text) VALUES ('Hello') ON CONFLICT DO NOTHING;
-INSERT INTO quotes (text) VALUES ('The quick brown fox jumps over the lazy dog.') ON CONFLICT DO NOTHING;
-INSERT INTO quotes (text) VALUES ('To be or not to be, that is the question.') ON CONFLICT DO NOTHING;
-INSERT INTO quotes (text) VALUES ('All that glitters is not gold.') ON CONFLICT DO NOTHING;
-INSERT INTO quotes (text) VALUES ('Practice makes perfect.') ON CONFLICT DO NOTHING;
-INSERT INTO quotes (text) VALUES ('Knowledge is power.') ON CONFLICT DO NOTHING;
-INSERT INTO quotes (text) VALUES ('Time flies when you''re having fun.') ON CONFLICT DO NOTHING;
+INSERT INTO quotes (text) VALUES ('S - Single Responsibility Principle') ON CONFLICT DO NOTHING;
+INSERT INTO quotes (text) VALUES ('O - Open/Closed Principle') ON CONFLICT DO NOTHING;
+INSERT INTO quotes (text) VALUES ('L - Liskov Substitution Principle') ON CONFLICT DO NOTHING;
+INSERT INTO quotes (text) VALUES ('I - Interface Segregation Principle') ON CONFLICT DO NOTHING;
+INSERT INTO quotes (text) VALUES ('D - Dependency Inversion Principle') ON CONFLICT DO NOTHING;
 INSERT INTO quotes (text) VALUES ('Actions speak louder than words.') ON CONFLICT DO NOTHING;
